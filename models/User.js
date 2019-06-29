753ff112be4cb4455ebefd7390584dd189c07b92
Siddharth <g.sidd97@gmail.com>
2019-06-29 05:53:42 -0700

    updated user model

--- models/User.js
+++ models/User.js
@@ -20,3 +20,37 @@ const UserSchema = new Schema({
 	}
 });
 module.exports = User = mongoose.model("users", UserSchema);
+
+// const mongoose = require('mongoose');
+// const scheme = mongoose.Schema;
+
+// /* Skill-set data object */
+// let skSet = new scheme({
+// 	type: String,
+// 	type_name: String,
+// 	length: Number,
+// 	description: String
+// });
+
+//  /* User data-object */
+// let User = new scheme({
+// 	UID: Number,
+// 	username: String,
+// 	password: String,
+// 	preferredName: String,
+// 	gender: String,
+// 	contacts:{
+// 		email: String,
+// 		github: String,
+// 		linkedIn: String
+// 	},
+// 	interests: [Boolean],
+// 	courses: [String],
+// 	skillset: [skSet],
+// 	bios: String,
+// 	availability:{
+// 		am: [Boolean],
+// 		pm: [Boolean],
+// 		pm2: [Boolean]
+// 	},
+// });
