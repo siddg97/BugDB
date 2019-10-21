315c8f7ea47a12f3a7f45a26a5f94cb49a696baf
Siddharth Gupta <g.sidd97@gmail.com>
2019-10-21 11:57:46 -0700

    Removed Semantic UI and added antd

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,10 @@
 import React from 'react';
 import './App.css';
-import 'semantic-ui-css/semantic.min.css'
-import { Route, Switch } from 'react-router-dom';
-import { Provider } from "react-redux";
-import store from "./store.js";
-import jwt_decode from 'jwt-decode';
-import setAuthToken from './utils/setAuthToken.js';
-import { setCurrentUser, logoutUser } from './redux/actions/authActions.js';
-
-import { Grid } from 'semantic-ui-react';
-import Login from './components/auth/Login.jsx';
-import Register from './components/auth/Register.jsx';
-import Landing from './components/layout/Landing.jsx';
-import PrivateRoute from './components/PrivateRoute.js';
-import Home from './components/Home.jsx';
-import NavBar from './components/layout/NavBar.jsx';
-
-if (localStorage.jwtToken) {
-	const token = localStorage.jwtToken;
-	setAuthToken(token);
-	const decoded = jwt_decode(token);
-	store.dispatch(setCurrentUser(decoded));
-
-	const currentTime = Date.now() /1000;
-	if(decoded.exp < currentTime) {
-		store.dispatch(logoutUser());
-		window.location.href = './login';
-	}
-}
 
 function App() {
   return (
-		<Provider store={store}>
-			<Grid stackable textAlign='center' style={{minHeight:'100vh',margin:0}} verticalAlign='top'>
-				<Grid.Row color='black'>
-					<Grid.Column width={16}>
-						<NavBar />
-						<Route exact path='/' component={Landing} />
-						<Route path='/login' component={Login} />
-						<Route path='/register' component={Register} />
-						<Switch>
-							<PrivateRoute exact path='/home' component={Home} />
-						</Switch>
-					</Grid.Column>
-				</Grid.Row>
-			</Grid>
-		</Provider>
+    <div className="App">
+    </div>
   );
 }
 
--- client/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducers.js';
-import errorReducer from './errorReducers.js';
-
-export default combineReducers({
-	auth: authReducer,
-	errors: errorReducer
-});
