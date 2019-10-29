da39ad0ecd69873ae82d61be3d555d6e8f4f8e5c
Siddharth <g.sidd97@gmail.com>
2019-10-28 19:58:09 -0700

    added antd config

---
+++ client/config-overrides.js
@@ -0,0 +1,15 @@
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+module.exports = override(
+	fixBabelImports('import', {
+		libraryName: 'antd',
+		libraryDirectory: 'es',
+		style: 'css',
+	}),
+	addLessLoader({
+		javascriptEnabled: true,
+		modifyVars: {
+			'@primary-color': '#1DA57A'
+		}
+	}),
+);
